aea3ae6a6decb0804a4a2db13865215bae590957
acidvegas <acid.vegas@acid.vegas>
2025-03-21 21:03:57 -0400

    version updated

--- czds/__init__.py
+++ czds/__init__.py
@@ -5,7 +5,7 @@
 from .client import CZDS
 
 
-__version__ = '1.2.7'
+__version__ = '1.2.8'
 __author__  = 'acidvegas'
 __email__   = 'acid.vegas@acid.vegas'
 __github__  = 'https://github.com/acidvegas/czds'
