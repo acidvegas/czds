2a7b271d4f54eaa1320e9a2296ffac3ae49b49d7
acidvegas <acid.vegas@acid.vegas>
2025-03-26 16:17:22 -0400

    fixed entry to library

--- czds/__init__.py
+++ czds/__init__.py
@@ -2,7 +2,9 @@
 # ICANN API for the Centralized Zones Data Service - developed by acidvegas (https://git.acid.vegas/czds)
 # czds/__init__.py
 
-__version__ = '1.3.6'
+from .client import CZDS
+
+__version__ = '1.3.7'
 __author__  = 'acidvegas'
 __email__   = 'acid.vegas@acid.vegas'
 __github__  = 'https://github.com/acidvegas/czds'
